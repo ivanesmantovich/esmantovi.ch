3357c93381f777f76ea48973eb4dbaff5890cc77
Ivan Esmantovich <ivanesmantovich@gmail.com>
2025-06-07 15:27:15 +0300

    Format the project

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,19 +1,19 @@
 export function formatDate(date: string) {
-  const noteDate = new Date(date);
-  const today = new Date();
-  const yesterday = new Date(today);
-  yesterday.setDate(yesterday.getDate() - 1);
+  const noteDate = new Date(date)
+  const today = new Date()
+  const yesterday = new Date(today)
+  yesterday.setDate(yesterday.getDate() - 1)
 
   if (noteDate.toDateString() === today.toDateString()) {
-    return "Today";
+    return 'Today'
   }
   if (noteDate.toDateString() === yesterday.toDateString()) {
-    return "Yesterday";
+    return 'Yesterday'
   }
 
-  return noteDate.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  return noteDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
 }
